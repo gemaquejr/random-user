d6f291a5b86546938b47d642976ede92a681f4cf
Francisco Espíndola Gemaque Junior <gemaquejr@hotmail.com>
2023-04-16 09:21:11 -0300

    Adiciona padronizacao em resquest.js

--- app/frontend/src/services/requests.js
+++ app/frontend/src/services/requests.js
@@ -4,11 +4,9 @@ const api = axios.create({
   baseURL: `http://localhost:${process.env.REACT_APP_API_PORT || '3001'}`,
 });
 
-const getAllUsers = async () => {
+export const getAllUsers = async () => {
   const response = await api.get('/users');
   return response.data;
 };
-
-export { getAllUsers };
 
 export default api;
