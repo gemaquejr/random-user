5600815aa215a95e115e797415f5dfc93ac73787
Francisco Espíndola Gemaque Junior <gemaquejr@hotmail.com>
2023-04-18 12:20:03 -0300

    Atualiza Componente UserList com o componente  Loading e acrescenta um delay de 1s para vermos o componente antes de carregar a API

--- app/frontend/src/components/UserList.jsx
+++ app/frontend/src/components/UserList.jsx
@@ -1,18 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import { getAllUsers } from '../services/requests';
 import Button from './Button';
+import Loading from './Loading';
 
 function UserList() {
   const [users, setUsers] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    async function fetchUsers() {
-      const response = await getAllUsers();
-      const { data } = response;
-      setUsers(data);
-    }
-    fetchUsers();
+    const delay = setTimeout(async () => {
+      try {
+        const response = await getAllUsers();
+        const { data } = response;
+        setUsers(data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error('Failed to fetch users:', error);
+        setIsLoading(false);
+      }
+    }, 1000);
+    return () => clearTimeout(delay);
   }, []);
 
   const handleNextUser = () => {
@@ -30,20 +38,24 @@ function UserList() {
   return (
     <div>
       <h1>Users</h1>
-      <ul>
-        {users.map((user, index) => (
-          index === currentIndex && (
-            <li key={user.id}>
-              <img src={user.photo} alt={user.name} />
-              <div>{user.name}</div>
-              <div>{user.email}</div>
-              <div>{user.phone}</div>
-              <div>{user.birthday}</div>
-              <div>{user.address}</div>
-            </li>
-          )
-        ))}
-      </ul>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <ul>
+          {users.map((user, index) => (
+            index === currentIndex && (
+              <li key={user.id}>
+                <img src={user.photo} alt={user.name} />
+                <div>{user.name}</div>
+                <div>{user.email}</div>
+                <div>{user.phone}</div>
+                <div>{user.birthday}</div>
+                <div>{user.address}</div>
+              </li>
+            )
+          ))}
+        </ul>
+      )}
       <div>
         <Button label="Previous" onClick={handlePreviousUser} />
         <Button label="Next" onClick={handleNextUser} />
