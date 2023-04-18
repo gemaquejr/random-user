9e974cf47d946894941358848fe1211b0885cf27
Francisco Espíndola Gemaque Junior <gemaquejr@hotmail.com>
2023-04-18 00:24:52 -0300

    Adiciona a configuracao do cors no servidor

--- app/backend/src/server.ts
+++ app/backend/src/server.ts
@@ -1,12 +1,20 @@
 import express from 'express';
 import 'dotenv/config';
 import routes from './routes';
+import cors from 'cors';
 
 const PORT = process.env.PORT || 3001;
+
 const app = express();
+app.use(express.json());
+app.use(cors());
+app.use((_req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+})
+
 app.use('/users', routes.userRouter);
 
-app.use(express.json());
 
 app.listen(PORT, ()=> {
     console.log(`Server is running on port: ${PORT}`)
