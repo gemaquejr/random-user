5c8f6767c5df27b1d047ec1e957258f8b50fa697
Francisco Espíndola Gemaque Junior <gemaquejr@hotmail.com>
2023-04-18 12:29:28 -0300

    Atualiza page Home

--- app/frontend/src/pages/Home.jsx
+++ app/frontend/src/pages/Home.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import Loading from '../components/Loading';
 import UserList from '../components/UserList';
 
 function Home() {
   return (
     <div>
-      <h1>Homepage</h1>
-      <Loading />
+      <h1>RANDOM USER GENERATOR</h1>
       <UserList />
     </div>
   );
